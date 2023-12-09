e1840471d88604bab4a484c3025dacd5a6109ee4
Sreedeepvs <153345558+Sreedeepvs@users.noreply.github.com>
2023-12-09 11:37:38 +0530

    made changes

--- Find email.py
+++ Find email.py
@@ -10,9 +10,9 @@ import re
 # In[ ]:
 
 
-sen1 = 'My email address is sreedeep@gmail.com'
-s=sen1.split(" ")
-s[-1]
+sen1  = 'My email address is sreedeep@gmail.com and my phone number is 9633159946'
+regex = '[a-z,0-9]+@gmail.com'
+re.findall(regex, sen1)
 
 
 # In[ ]:
