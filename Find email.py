cd244e648dd43063bc88534ba90907ef9da587b0
Sreedeepvs <153345558+Sreedeepvs@users.noreply.github.com>
2023-12-09 11:35:20 +0530

    Add files via upload

---
+++ Find email.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import re
+
+
+# In[ ]:
+
+
+sen1 = 'My email address is sreedeep@gmail.com'
+s=sen1.split(" ")
+s[-1]
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
