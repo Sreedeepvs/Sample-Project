69e301ad95730debf11d4ceee821bfffe3280e4a
Sreedeepvs <153345558+Sreedeepvs@users.noreply.github.com>
2023-12-09 11:36:38 +0530

    Made changes

---
+++ Find email (1).py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import re
+
+
+# In[ ]:
+
+
+sen1  = 'My email address is sreedeep@gmail.com and my phone number is 9633159946'
+regex = '[a-z,0-9]+@gmail.com'
+re.findall(regex, sen1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
